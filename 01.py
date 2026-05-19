0a6d16162c3f1477a3f469614d41dc9e72d9782a
Yogesh Rathod <117069440+yogeshrathod04@users.noreply.github.com>
2026-05-19 22:28:06 +0530

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,10 +1,1 @@
-from playwright.sync_api import sync_playwright
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://www.google.com")
-    print("Chrome successfully opened")
-    print(page.title())
-
-    page.wait_for_timeout(3000)
