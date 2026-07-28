fa7c165732536cbe3cdf4daba05a5f168fe7a331
Yogesh Rathod <117069440+yogeshrathod04@users.noreply.github.com>
2026-07-28 16:05:32 +0530

    Add Playwright script for radio and checkbox actions

--- 05_RadioButton_CheckBox.py
+++ 05_RadioButton_CheckBox.py
@@ -1,1 +1,29 @@
 
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://demo.automationtesting.in/Register.html")
+    # page.select_option("//select[@id='country']", "India")
+    # page.select_option("//select[@id='yearbox']", "2004")
+    # page.wait_for_selector("input[type='password']").fill("12345678")
+    # page.wait_for_selector("//select[@placeholder='Month']").select_option('January')
+
+    # Radio Button
+    radio_button = page.query_selector('//input[@value="FeMale"]')
+    radio_button.click()
+    # page.query_selector('//input[@Value="FeMale"]').check()
+    # page.query_selector('input[value="FeMale"]').set_checked(True)
+
+    # Check Box
+    page.query_selector('input[value="Cricket"]').set_checked(True)
+
+    check_box = page.query_selector("//input[@value='Movies']")
+    check_box.click()
+
+    page.query_selector("input[value='Hockey']").click()
+
+    page.wait_for_timeout(5000)
+
